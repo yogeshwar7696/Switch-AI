bed660074003145636ee478e0d01ef90b53971a5
Yogeshwar <bonthuyogeshwar@gmail.com>
2025-06-17 15:46:51 +0530

    Update Main.jsx

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useRef, useState } from "react";
 import "./Main.css";
 import { assets } from "../../assets/assets";
-import { Context } from "../../context/Context";
+import { Context } from "../../context/Context.jsx";
 
 const main = () => {
     const { setSelectedOption, selectedOption,onSent, recentPrompt, showResult, resultData, loading, setInput, input } = useContext(Context);
